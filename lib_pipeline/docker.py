d0495ecfe4ddbb2e230fbbf13e627c6c2ff46150
balassit <blakelass@gmail.com>
2019-03-10 12:42:36 -0700

    remove pull docker before run

--- lib_pipeline/docker.py
+++ lib_pipeline/docker.py
@@ -10,7 +10,6 @@ class Docker(object):
         return execute(f"docker pull {image}")
 
     def run(self, image, *commands, cwd=None, env_args="", options=""):
-        self.pull(image)
         command = " ".join(arg for arg in commands)
         return execute(
             f"""docker run -it -v $(pwd):/home/src -w /home/src {env_args} {image} {command} {options}""",
