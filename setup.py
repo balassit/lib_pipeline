cf7968c8364065d688c9a16af6e80c13620fbe6c
Blake Lassiter <blake.lassiter@capitalone.com>
2020-04-04 12:09:11 -0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lib_pipeline",
-    version="0.0.10",
+    version="0.0.11",
     author="Blake Lassiter",
     author_email="blakelass@gmail.com",
     description="A python implementation of services to help deploy using aws and terraform",
